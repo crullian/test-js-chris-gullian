66c940460278f060a10c96eabb51af4047209993
crullian <crullian@gmail.com>
2016-09-26 18:59:49 -0400

    Format and sort json response data from the api

--- src/state/actions.js
+++ src/state/actions.js
@@ -7,6 +7,22 @@ export function getPopularMovies () {
     const fiveStarUrl = 'https://itunes.apple.com/search?country=us&media=movie&entity=movie&limit=100&attribute=ratingIndex&term=5'
     const req1 = fetch(fourStarUrl)
     const req2 = fetch(fiveStarUrl)
+    const compare = (a, b) => {
+      let a_trackName = a.trackName.replace('The ', '')
+      let b_trackName = b.trackName.replace('The ', '')
+      if (a.releaseYear < b.releaseYear) {
+        return -1;
+      } else if (a.releaseYear > b.releaseYear) {
+        return 1;
+      }
+      if (a_trackName < b_trackName) {
+        return -1;
+      } else if (a_trackName > b_trackName) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
 
     return Promise.all([req1, req2])
       .then(responses => responses.map(res => res.json()))
@@ -23,7 +39,17 @@ export function getPopularMovies () {
         //    this is used in src/components/movies-list.js line 26
         //
 
-        const combinedResults = []
+        // Lets build a new array of the results data.
+        const jsonResultSet = jsonResponses[0].results.concat(jsonResponses[1].results)
+
+        // This maps a new year property on each entry derived from 'releaseDate' time stamp.
+        jsonResultSet.map(entry => {
+          return entry['releaseYear'] = moment(entry['releaseDate']).format('YYYY')
+        });
+
+        // Here we set our final results array by sorting our results data (if we have it!)
+        // using our custom compare function which sorts first by year, then by title.
+        const combinedResults = jsonResultSet ? jsonResultSet.sort(compare) : []
 
         return dispatch({
           type: 'GET_MOVIES_SUCCESS',
